e8d652fc0caf144b50519e281fdf9fe9e2f0c558
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-01-31 18:06:04 +0100

    Resolved first issue

--- Regression.py
+++ Regression.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn as seabornInstance
+import matplotlib.pyplot as plt
+import seaborn as seabornInstance
 # from sklearn.model_selection import train_test_split
 # from sklearn.linear_model import LinearRegression
 # from sklearn import metrics
@@ -17,7 +17,7 @@ plt.xlabel('Outdoor Drybulb Temperature')
 plt.ylabel('Outdoor Relative Humidity')
 plt.show()
 
-plt.figure(figuize=(15,10))
+plt.figure(figuize=(15, 10))
 plt.tight_layout()
 seabornInstance.distplot(dataset('Outdoor Relative Humidity'))
 plt.show()
