8b834484aff752728a9b0cc6b554ec7b4c775b17
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-02-01 23:30:37 +0100

    model built sucessfully

--- Regression.py
+++ Regression.py
@@ -1,25 +1,47 @@
 import pandas as pd
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as seabornInstance
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
-# from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 #matplotlib inline
-dataset = pd.read_csv(r'C:\Users\USER\MachineLearning\weather\weather_data.csv')
+dataset = pd.read_csv(r'C:\Users\USER\MachineLearning\weather\weatherdata.csv')
 print(dataset.shape)
 
 print(dataset.describe())
 
-dataset.plot(x='Outdoor Drybulb Temperature [C]', y='Outdoor Relative Humidity [%]', style='o')
-plt.title('Outdoor Drybulb Temperature Vs Outdoor Relative Humidity')
-plt.xlabel('Outdoor Drybulb Temperature')
-plt.ylabel('Outdoor Relative Humidity')
+dataset.plot(x='Minimum temperature', y='Maximum temperature', style='o')
+plt.title('Minimum temperature Vs  Maximum temperature')
+plt.xlabel('Minimum temperature')
+plt.ylabel('Maximum temperature')
 plt.show()
 
-plt.figure(figuize=(15, 10))
+plt.figure(figsize=(15, 10))
 plt.tight_layout()
-seabornInstance.distplot(dataset('Outdoor Relative Humidity'))
+seabornInstance.distplot(dataset('Maximum temperature'))
 plt.show()
 
 #dataslicing
+X = dataset['Minimum temperature'].values.reshape(-1, 1)
+Y = dataset['Maximum temperature'].values.reshape(-1, 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train) #training the algorithm
+
+#To retrieve the intercept:
+print('Intercept', regressor.intercept_)
+print('Coefficient', regressor.coef_)
+
+y_prod = regressor.predict(X_test)
+
+df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+print(df)
+
+dfl = df.head(25)
+dfl.plot(kind='bar', figsize=(16, 10))
+plt.grid(whiich='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
