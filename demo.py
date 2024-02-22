31a134bd51dc3bcf70dbf546316b01701a7f6977
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-02-22 11:08:33 +0100

    Styling the matplotlib

--- demo.py
+++ demo.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
+from matplotlib import  style
 
+style.use("ggplot")
 #Plotting our canvas
 x = [5,8,9]
 y = [12,16,6]
-plt.plot(x,y)
 
-#Showing what we plotted
+x2 = [6,9,11]
+y2 = [6,15,7]
+
+plt.plot(x,y,'g',label='line one', linewidth=5)
+plt.plot(x2,y2,'c',label='line two', linewidth=5)
+#
+# #Showing what we plotted
 plt.title("Demo graph")
 plt.xlabel("Minumum Temperature")
 plt.ylabel("Maximum Temperature")
+
+plt.legend() #to add legend information to the top of your graph
+plt.grid(True, color='k') #to  add grid line
 plt.show()
+
+#to add style to the graph
