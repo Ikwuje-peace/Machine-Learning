e77a7be3891e88f2904bf6d9112e7bed387ecb5a
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-02-24 13:30:04 +0100

    Pie Plot added

---
+++ pieplot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+slices = [7,2,2,13]
+activities = ['sleeping', 'eating', 'working', 'playing']
+cols = ['c', 'm', 'r', 'b']
+
+plt.pie(slices, 
+        labels=activities, 
+        colors=cols, 
+        startangle=90,  
+        hadow=True,  
+        explode=(0,0.1,0,0),  
+        autopct='%1.1f%%' )
+
+plt.title('Pie Plot')
+plt.show()
