71491574e687ccfdcf3f690598e9156dea8aa749
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-02-26 10:17:08 +0100

    more array items

--- Numpy/demo1.py
+++ Numpy/demo1.py
@@ -18,3 +18,17 @@ print("type", b.dtype, "Size", b.itemsize, "bytes")
 print("Dimension for b", b.ndim, b.shape)
 
 #floats are bigger than integers
+#Get a specific element [rows, column] start python indexing at zero
+print(b[0, 2])
+
+#to get a specific row, it is simple just use [row :] the : sign is for slicing
+print(b[1 :])
+
+#to get a specific column, it is simple too just use [:, column] 
+#the comma after the slicing is important because if it is not there, it is just going to print out all of the first row
+print(b[:, 1])
+ 
+#Getting a little more fancy [startindex:endindex:stepsize]
+c = np.array([[1,2,3,4,5,6,7], [8,9,10,11,12,13,14]])
+
+print(c[0, 1:6:2])
