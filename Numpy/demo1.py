f27c22d45282feb114aad0a3ae8ca6e4acebaa90
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-02-26 10:01:40 +0100

    Learning more about Numpy arrays

--- Numpy/demo1.py
+++ Numpy/demo1.py
@@ -8,7 +8,13 @@ b = np.array([[1.0,2.0,3.0],[4.0,5.0,6.0]])
 print(a)
 
 #get Dimension
-print("Dimenison",a.ndim)
+print("Dimenison",a.ndim, a.shape, a.dtype, a.itemsize)
+#to get the total size of the array
+#print(a.size*a.itemsize) or
+print(a.nbytes)
 
 #Get Type/Size
 print("type", b.dtype, "Size", b.itemsize, "bytes")
+print("Dimension for b", b.ndim, b.shape)
+
+#floats are bigger than integers
