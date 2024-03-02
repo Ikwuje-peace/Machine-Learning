03643b4f7cfe038eddb06b82d4692a490f20ab97
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-02 18:05:43 +0100

    Simple Mathematics with Numpy

---
+++ Numpy/mathematics.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array([1,2,3,4])
+#adds 2 to every element in the array
+a *= 2
+# same way you can minus too -=2, or *=2 or =+2
+#to take sin of all the numbers
+b = np.sin(a)
+print(a)
+print(b)
