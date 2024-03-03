147fcf5a42f160bdbc0137ad4c31f61b00bba806
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-03 13:43:58 +0100

    Statistics with numpy

---
+++ Numpy/statistics.py
@@ -0,0 +1,8 @@
+#Statistics with Numpy
+import numpy as np
+
+stats = np.array([[1,2,3],[5,6,7]])
+minimum_number = np.min(stats, axis=0)
+maximum_number = np.max(stats)
+print(maximum_number)
+print(minimum_number)
