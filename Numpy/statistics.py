497e196881e00c1b3de4408b104282d07ba16aed
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-03 13:45:35 +0100

    Statistics with numpy

--- Numpy/statistics.py
+++ Numpy/statistics.py
@@ -4,5 +4,7 @@ import numpy as np
 stats = np.array([[1,2,3],[5,6,7]])
 minimum_number = np.min(stats, axis=0)
 maximum_number = np.max(stats)
+sum = np.sum(stats)
+print(sum)
 print(maximum_number)
 print(minimum_number)
