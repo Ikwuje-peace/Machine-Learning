6c10fd62b8d6dc5ebf88d6ce3e047b8606215490
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-04 09:36:02 +0100

    Verticall and horizontally stacks

--- Numpy/Reorganizingarrays.py
+++ Numpy/Reorganizingarrays.py
@@ -4,3 +4,11 @@ before = np.array([[1,2,3,4],[5,6,7,8]])
 print(before)
 after = before.reshape(8,1)
 print(after)
+
+#Vertically stacking vectors and matrix
+v1 = np.array([1,2,3,4])
+v2 = np.array([5,6,7,8])
+print(np.vstack([v1,v2, v2]))
+h1 = np.ones((2,4))
+h2 = np.zeros((2,2))
+print(np.hstack([h1,h2]))
