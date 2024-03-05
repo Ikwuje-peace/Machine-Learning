fc591d28ff58eb5ed186eceae6e596c02cfc0410
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-05 11:29:57 +0100

    Thee load data now works and is understandable

--- Numpy/loaddata.py
+++ Numpy/loaddata.py
@@ -1,5 +1,11 @@
 import numpy as np
+import pandas as pd
 
 #to load data from a file to a numpy array
-data= np.genfromtxt('data.txt', delimiter=',')  #doesn't work yet
+data = pd.read_csv(r'C:\Users\USER\MachineLearning\Numpy\data.csv', delimiter=',')
+data_set = np.genfromtxt(r'C:\Users\USER\MachineLearning\Numpy\data.txt', delimiter=',', dtype='int8')
+
+#This works now
 print(data)
+print('The data from the numpy is ')
+print(data_set)
