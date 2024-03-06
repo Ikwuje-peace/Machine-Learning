6624aaf679a91513ae5fa664023e8917da84746b
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-06 09:50:23 +0100

    Seaborn instance 2

--- Seaborn/distribution.py
+++ Seaborn/distribution.py
@@ -3,6 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-weather = pd.read_csv(r'C:\Users\USER\MachineLearning\Numpy\data.csv')
-crash_df = sns.load_dataset('weather')
-print(crash_df)
+plot = sns.load_dataset('mpg')
+print(plot.head())
