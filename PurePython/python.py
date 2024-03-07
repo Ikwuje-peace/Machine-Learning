00197f65ba0b9000c061fe5cd3f43b50bd126a38
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-07 21:52:44 +0100

    The init method for the class

--- PurePython/python.py
+++ PurePython/python.py
@@ -20,3 +20,12 @@ def scope_test():
 
 scope_test()
 print("In global scope:", spam)
+
+
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+x = Complex(3.0, 4.5)
+print(x.r, x.i)
