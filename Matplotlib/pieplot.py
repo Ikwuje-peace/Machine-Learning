7484809d3739552624abf6c9970a97bb5bc36b38
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-12 23:00:22 +0100

    Multiple pie chart plots

--- Matplotlib/pieplot.py
+++ Matplotlib/pieplot.py
@@ -8,9 +8,22 @@ plt.pie(slices,
         labels=activities, 
         colors=cols, 
         startangle=90,  
-        hadow=True,  
+        shadow=True,  
         explode=(0,0.1,0,0),  
         autopct='%1.1f%%' )
 
 plt.title('Pie Plot')
+
+partitions = [45, 30, 15, 10]
+names_of_section = ['Prayer', 'Reading', 'Coding', "Sleeping"]
+colrs = ['k', 'c', 'r', 'b']
+
+plt.pie(partitions, explode=(0,3,0,6.2), labels=names_of_section, colors=colrs, pctdistance=0.6, shadow=True, radius=20, )
+
+plt.subplot(211)
+plt.plot()
+
+plt.subplot(212)
+plt.plot()
+
 plt.show()
