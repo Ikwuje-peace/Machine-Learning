a6e8937ea3d44f2ac01b773ae671bca748c2308e
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-13 21:20:39 +0100

    Makinng the game better

--- PurePython/input.py
+++ PurePython/input.py
@@ -1,3 +1,15 @@
+#Let's create a simple game
+import random
 
+number = random.randint(1, 100)
 name = input(str("What is your name "))
-print("Welcome {}".format(name))
+print("Welcome {} the game starts now".format(name))
+guess = input(int())
+
+if guess == number:
+    print('You got the number right')
+else:
+    print("{} is the correct number".format(number))
+
+
+#Let's create a simple game
