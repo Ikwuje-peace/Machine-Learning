4d91ed373502fc04f603c0dbdd31047970d0d989
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-13 21:04:19 +0100

    Usinng the float method

--- PurePython/random.py
+++ PurePython/random.py
@@ -1,4 +1,9 @@
 import random
 
 number = random.randint(10, 80)
+
+if (number // 2 )== 0:
+    print("{} is an even number".format(number))
+else:
+    print("{} is not an even number".format(number))
 print(number)
