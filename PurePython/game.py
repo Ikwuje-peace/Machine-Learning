78ee0fda5d74f5f7141c771aeaf44c0fe8b16496
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-15 22:52:39 +0100

    The game is noe functional

--- PurePython/game.py
+++ PurePython/game.py
@@ -1,17 +1,19 @@
 import random
 
-print('This is a guessning game')
+print('This is a guessing game')
 
-number_of_guess = 5
+trials = 5
 score = 0
 number = random.randint(1, 100)
 name = input(str('What is your name '))
 print('{}, Thank you for agreeing to do this game with us'.format(name))
 print('You have five guesses, try to guess the number')
 print(number)
-first_guess = input(int())
+guess = input(int())
 
-if first_guess == number:
-    number_of_guess = 5
-    score = 2
-    print(score)
+if number == guess:
+    score =+ 5
+    print('{0}, is your score {1}'.format(score, name))
+else:
+    score = 0
+    print('You didnt guess correctly')
