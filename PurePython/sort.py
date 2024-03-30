b83850ff1b8bae396be1c281025901e8dd022cf4
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-30 22:00:41 +0100

    using a function to sort the list

--- PurePython/sort.py
+++ PurePython/sort.py
@@ -3,3 +3,13 @@ fruits = ['mango', 'apple', 'guava', 'orange', 'banana', 'watermelon']
 #This function below sorts the list of fruits in alphabetical order
 fruit = sorted(fruits)
 print(fruit)
+
+"""Or"""
+class Numbers():
+    def __init__(self, list):
+        self.list = []
+        self.desc = sorted(list)
+
+        
+x = Numbers([23,41,34,54,26,45,67])
+print(x.desc)
