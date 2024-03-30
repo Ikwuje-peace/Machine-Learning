1380d434c19e1d0e18b7292358867f13028bb700
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-03-30 21:45:30 +0100

    Just sorting a list

---
+++ PurePython/sort.py
@@ -0,0 +1,5 @@
+"""This file is to create a simple function that sorts a list"""
+fruits = ['mango', 'apple', 'guava', 'orange', 'banana', 'watermelon']
+#This function below sorts the list of fruits in alphabetical order
+fruit = sorted(fruits)
+print(fruit)
