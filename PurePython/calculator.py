d96bce401ba657bb222f970c48d9231201152174
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-04-01 22:16:57 +0100

    Calculator function added

---
+++ PurePython/calculator.py
@@ -0,0 +1,13 @@
+class Calculator():
+    def __init__(self, num1, operation, num2):
+        if operation == '+':
+            print(num1+num2)
+        elif operation == '-':
+            print(num1-num2)
+        elif operation == '/':
+            print(num1/num2)
+        elif operation == '*':
+            print(num1*num2)
+
+x = Calculator(13, '-', 2)
+print(x)
