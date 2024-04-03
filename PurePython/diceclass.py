54238723a3896f03d2a1b071483224f741161439
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-04-03 19:09:54 +0100

    Dice game complete

--- PurePython/diceclass.py
+++ PurePython/diceclass.py
@@ -1,6 +1,6 @@
 import random
 
-class Dice:
+class Dice():
     def __init__(self, numberOfSides=6):
         self._numberOfSides = numberOfSides
         self.value =  1
@@ -8,3 +8,20 @@ class Dice:
     def throw(self):
         self.value = random.randint(1, self._numberOfSides)
         return self.value
+    
+dice1 = Dice(6)
+dice1.throw()
+print("\n First Dice: ")
+print(dice1.value)
+
+dice2 =  Dice(6)
+dice2.throw()
+print('\n Second Dice')
+print(dice2.value)
+
+if dice1.value > dice2.value:
+    print('First dice wins')
+elif dice1.value < dice2.value:
+    print('Second dice')
+else:
+    print('It is a draw')
