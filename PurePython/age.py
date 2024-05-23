f2df4929a8878b69c419ddac577f9a4fc178e7c2
Ikwuje-Peace <peaceikwuje21@gmail.com>
2024-05-23 17:07:09 +0100

    Commit message

---
+++ PurePython/age.py
@@ -0,0 +1,7 @@
+#Write a program with a variable called age assigned to an integer that prints different
+#strings depending on what integer age is
+
+age = 1
+for i in range(1, 21):
+    age += 1
+    print('Age is {}'.format(age))
