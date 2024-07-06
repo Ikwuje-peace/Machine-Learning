984b103981af8e79d14c886107d814b50ec168b3
Ikwuje-peace <49587371+Ikwuje-peace@users.noreply.github.com>
2024-07-06 23:25:14 +0100

    Update age.py

--- PurePython/age.py
+++ PurePython/age.py
@@ -3,4 +3,5 @@
 
 age = int(input('How Old are you'))
 name = str(input('What is your name'))
-print("Welcome {name} to the award giving show. You are {age} years old. lease enter")
+print("Welcome {name} to the award giving show. You are {age} years old. Please enter")
+print("You are onlh eligible to enter if your past legal age")
